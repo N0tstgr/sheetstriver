60ceea98640e5d46336520749303ed9819468b02
Notstranger <vikashkasaudhan321@gmail.com>
2026-09-02 18:54:11 +0530

    Implement uniformArray method in Solution class

---
+++ IKbutsolvebyoptimized.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int[] nums2 =  new int[nums1.length];
+        for(int i = 0; i<nums1.length; i++){
+            nums2[i] = nums1[i];
+            if(nums2[i] == nums1[i]){
+                return true;
+                } 
+        }
+
+        return false;
+
+    }
+}
